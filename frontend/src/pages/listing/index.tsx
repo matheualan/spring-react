a864a7293583219710c6541223cbee2b895c0f37
Matheus Alan <matheusalanpv@outlook.com>
2022-05-10 20:41:17 -0300

    MovieScore and MovieStars

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,13 @@
+import MovieScore from "components/movie-score";
 import Pagination from "components/pagination";
 
 function Listing() {
-    return (
-        <Pagination />
-    );
+  return (
+    <>
+      <Pagination />
+      <MovieScore />
+    </>
+  );
 }
 
 export default Listing;
