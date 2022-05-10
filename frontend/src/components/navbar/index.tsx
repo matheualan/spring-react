64cab8631603aa3ee26afcff241f36319122bebb
Matheus Alan <matheusalanpv@outlook.com>
2022-05-10 15:39:43 -0300

    Personalização NavBar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,21 +1,22 @@
-import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import './styles.css'
 
 function Navbar() {
-    return (
-        <header>
-        <nav>
-          <div>
-            <h1>DS Movie</h1>
-            <a href="https://github.com/matheualan">
-              <div>
-                <GithubIcon />
-                <p>/matheualan</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DS Movie</h1>
+          <a href="https://github.com/matheualan">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/matheualan</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
