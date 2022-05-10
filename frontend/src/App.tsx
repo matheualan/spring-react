6ec953a0892d2e561639df0a1fda8b0cd04f36d2
Matheus Alan <matheusalanpv@outlook.com>
2022-05-10 16:01:19 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import { ReactComponent as GithubIcon } from './assets/img/github.svg';
-import Navbar from './components/navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/form';
+import Navbar from "components/navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
