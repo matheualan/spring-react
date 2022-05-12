0dcf8680646f0304fba2959514dccb5c6284632e
Matheus Alan <matheusalanpv@outlook.com>
2022-05-11 21:31:54 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String email;
+
+    public User() {
+    }
+
+    public User(UUID id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
